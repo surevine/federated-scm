fb8aaa5f7336528e1a5fd25314e984d4411d5750
JonnyHeavey <jonnyheavey@gmail.com>
2015-02-09 14:08:40 +0000

    renamed missed logger

--- src/main/java/com/surevine/gateway/scm/scmclient/gitlab/GitlabDeleteProjectCommand.java
+++ src/main/java/com/surevine/gateway/scm/scmclient/gitlab/GitlabDeleteProjectCommand.java
@@ -19,27 +19,27 @@ import com.surevine.gateway.scm.util.SCMSystemProperties;
 
 public class GitlabDeleteProjectCommand extends AbstractGitlabCommand implements DeleteRepoCommand {
 
-    private static Logger logger = Logger.getLogger(GitlabDeleteProjectCommand.class);
+    private static final Logger LOGGER = Logger.getLogger(GitlabDeleteProjectCommand.class);
     private static final String RESOURCE = "/api/v3/projects/";
     private SCMSystemProperties scmSystemProperties;
 
     GitlabDeleteProjectCommand() {
         scmSystemProperties = PropertyUtil.getSCMSystemProperties();
     }
-	
+
 	public void deleteRepo(String projectKey, String repoSlug) throws SCMCallException {
-		
+
         String resource = scmSystemProperties.getHost() + RESOURCE;
         try {
 			resource += URLEncoder.encode(projectKey+"/"+repoSlug, "UTF-8");
 		} catch (UnsupportedEncodingException e) {
             throw new SCMCallException("deleteRepo", "Could not build DELETE URL, UTF-8 encoding not supported");
 		}
-        
+
         String privateToken = scmSystemProperties.getAuthToken();
         Client client = getClient();
-        logger.debug("REST call to " + resource);
-        
+        LOGGER.debug("REST call to " + resource);
+
         GitlabProjectJSONBean createdBean = null;
         try {
         	client.target(resource)
@@ -47,7 +47,7 @@ public class GitlabDeleteProjectCommand extends AbstractGitlabCommand implements
                 .request(MediaType.APPLICATION_JSON)
                 .delete();
         } catch (ProcessingException pe) {
-            logger.error("Could not connect to REST service " + resource, pe);
+            LOGGER.error("Could not connect to REST service " + resource, pe);
             throw new SCMCallException("deleteRepo", "Could not connect to REST service:" + pe.getMessage());
         } finally {
             client.close();
